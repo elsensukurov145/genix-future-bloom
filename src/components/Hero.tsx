62dd4f1148a4523711f76e52e444aee10606dfa1
Elshen Shukurov <shukurovelshan388@gmail.com>
2026-04-19 02:45:07 +0400

    Update HEADING to include line break

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ import { ArrowRight, ArrowDown } from "lucide-react";
 
 const LampScene = lazy(() => import("./three/LampScene"));
 
-const HEADING = "Gələcəyin liderləri buradan başlayır";
+const HEADING = "Gələcəyin liderləri <br/> buradan başlayır";
 
 export const Hero = () => {
   const [on, setOn] = useState(false);
